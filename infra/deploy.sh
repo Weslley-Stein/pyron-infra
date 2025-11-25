f95b395ad32374a790b167d48a6233be6e468e65
weslley-stein <weslley.stein@gmail.com>
2025-11-26 05:27:04 +0800

    fix: switch to official docker repo

--- infra/deploy.sh
+++ infra/deploy.sh
@@ -25,7 +25,25 @@ echo "Starting deployment script..."
 
 echo "Updating packages..."
 apt update -y && apt upgrade -y
-apt install -y docker.io docker-compose nginx certbot python3-certbot-nginx dnsutils
+# Uninstall conflicting packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do apt-get remove -y $pkg; done
+
+# Add Docker's official GPG key:
+apt-get update
+apt-get install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+apt-get update
+
+# Install Docker and plugins
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nginx certbot python3-certbot-nginx dnsutils
 
 echo "Enabling services..."
 systemctl enable --now docker
